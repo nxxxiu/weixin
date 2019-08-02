8030e89a287b015951f048969456eb1af839fbe5
nxxxiu <1623248430@qq.com>
2019-08-02 02:15:18 +0000

    任务调度

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->call(function (){
-            "http://video.cccute.top/weixin/sendd";
+            'curl http://video.cccute.top/weixin/sendd';
         })->cron('* * * * *');
     }
 
