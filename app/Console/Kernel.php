356d4a0ecfc0569870192edc09b1d07545ee5ebc
nxxxiu <1623248430@qq.com>
2019-08-02 02:32:08 +0000

    curl

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,9 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->call(function (){
-            curl('http://video.cccute.top/weixin/sendd');
-        })->cron('* * * * *');
+        $schedule->exec('curl"http://video.cccute.top/weixin/sendd"')->everyMinute();
     }
 
     /**
